8ef9f019adcaf0f78939e23e5fbf48faf07c9db2
Mani Poorna <ndmprao2001@gmail.com>
2023-09-19 08:33:15 +0530

    V 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  const [show,setShow] = useState("hide");
+  const changePara =()=> {
+    show==="hide" ? setShow("show") : setShow("hide");
+  }
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <button id="click" onClick={changePara}>Click</button>
+      <p className={show} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+
     </div>
   );
 }
